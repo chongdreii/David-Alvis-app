4db4644cbc7a0f6ce040e78287354554c87e7c42
chongdreii <alvis_davidandrei@plpasig.edu.ph>
2026-02-20 22:27:04 +0800

    Added test route

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,3 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\UserController;
-use App\Services\UserService;
-use Illuminate\Support\Facades\Response;
-
-
-Route::get('/', function () {
-    return 'hello world!';
-});
-
-Route::get('show-users', [UserController::class, 'show']);
-
-//service container
-Route::get('/test-container', function (Request $request) {
-    $input = $request->input('key');
-    return $input;
-});
-
-//service providers
-Route::get('/test-provider', function (UserService $userService) {
-    return $userService->listUsers();
-});
-
-//service Provider
-Route::get('/test-users', [UserController::class, 'index']);
-
-// Facades
-Route::get('test-facade', function (UserService $userService) {
-    return Response::json($userService->listUsers());
+Route::get('/test', function () {
+    return 'Hello GitHub';
 });
