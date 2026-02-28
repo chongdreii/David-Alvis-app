a84abd5bd7d4f2917b38ae1d501ac3b2b5db3290
chongdreii <alvis_davidandrei@plpasig.edu.ph>
2026-02-28 17:22:42 +0800

    Exercise #4 - Views and Controller

--- routes/web.php
+++ routes/web.php
@@ -2,74 +2,153 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\UserController;
-use App\Services\UserService;
 use Illuminate\Support\Facades\Response;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return 'hello world!';
+use App\Services\UserService;
+use App\Services\ProductService;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+
+// Home Route
+Route::get('/', function (Request $request) {
+
+    // get name from query string (?name=David)
+    $name = $request->input('name', 'Greed Island');
+
+    return view('welcome', compact('name'));
+
 });
 
-Route::get('show-users', [UserController::class, 'show']);
 
-//service container
+// Show users using controller
+Route::get('/show-users', [UserController::class, 'show']);
+
+
+// Service Container example
 Route::get('/test-container', function (Request $request) {
+
     $input = $request->input('key');
+
     return $input;
+
 });
 
-//service providers
+
+// Service Provider example
 Route::get('/test-provider', function (UserService $userService) {
+
     return $userService->listUsers();
+
 });
 
-//service Provider
+
+// Controller route
 Route::get('/test-users', [UserController::class, 'index']);
 
-// Facades
-Route::get('test-facade', function (UserService $userService) {
+
+// Facade example
+Route::get('/test-facade', function (UserService $userService) {
+
     return Response::json($userService->listUsers());
+
 });
 
 
-Route::get('/post/{post}/comment/{comment}', function (string $postId, string $commentId){
-    return "Post ID: " . $postId . "- Comment ID: " . $commentId;
+// Route parameters
+Route::get('/post/{post}/comment/{comment}', function ($post, $comment) {
+
+    return "Post ID: " . $post . " - Comment: " . $comment;
+
 });
 
-Route::get('/post/{id}', function (string $id){
+
+// Route with number only constraint
+Route::get('/post/{id}', function ($id) {
+
     return $id;
+
 })->where('id', '[0-9]+');
 
-Route::get('/search/{search}', function (string $search){
+
+// Route accept any search string
+Route::get('/search/{search}', function ($search) {
+
     return $search;
+
 })->where('search', '.*');
 
+
+// Named route
 Route::get('/test/route/sample', function () {
+
     return route('test-route');
+
 })->name('test-route');
 
 
+// Middleware group
 Route::middleware(['user-middleware'])->group(function () {
-    Route::get('route-middleware-group/first', function (Request $request) {
-       echo "First Route";
+
+    Route::get('route-middleware-group/first', function () {
+
+        return 'first';
+
     });
 
-    Route::get('route-middleware-group/second', function (Request $request) {
-        echo "Second Route";
-     });
+    Route::get('route-middleware-group/second', function () {
+
+        return 'second';
+
+    });
+
 });
 
+
+// Controller group
 Route::controller(UserController::class)->group(function () {
-    Route::get('/users','index');
+
+    Route::get('/users', 'index');
+
     Route::get('/users/first', 'first');
-    Route::get('/users/{id}', 'get'); 
+
+    Route::get('/users/{id}', 'get');
+
 });
 
-Route::get('/token', function (Request $request) {
+
+// CSRF form
+Route::get('/token', function () {
+
     return view('token');
+
 });
+
 
 Route::post('/token', function (Request $request) {
+
     return $request->all();
+
+});
+
+
+// Resource Controller
+Route::resource('products', ProductController::class);
+
+
+// View with data from service
+Route::get('/product-list', function (ProductService $productService) {
+
+    $products = $productService->listProducts();
+
+    return view('product-list', compact('products'));
+
 });
